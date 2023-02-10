81a40f972f8f875ceea60be94fe6c5c6e0307830
diogo <diihrocha143@gmail.com>
2023-02-09 22:35:47 -0300

    primeiro commit

---
+++ script/main.js
@@ -0,0 +1,64 @@
+const img = document.getElementById("img")
+
+const imgs = document.querySelectorAll("#img img")
+
+let idx = 0;
+
+function carrossel(){
+	idx++;
+	
+	if(idx > imgs.length - 1){
+		idx = 0;
+	}
+	
+	img.style.transform = `translateX(${-idx * 100}%)`
+}
+setInterval(carrossel, 2300)
+
+ let posicao = document.getElementById("sobreMim")
+
+ //217.90625
+ let p = posicao.getBoundingClientRect()
+
+ console.log(p)
+
+ let sobreMim = document.getElementById("sobre")
+
+ let oqFaco = document.getElementById("faco")
+
+ let projeto = document.getElementById("projeto")
+
+ let objetivo = document.getElementById("objetivo")
+
+ let contato = document.getElementById("contato")
+
+ sobreMim.addEventListener("click", function() {
+
+	window.scrollTo(0, 420)
+	
+ })
+ oqFaco.addEventListener("click", function() {
+
+	window.scrollTo(0, 760)
+	
+ })
+
+ projeto.addEventListener("click", function() {
+
+	window.scrollTo(0, 1103.53125)
+	
+ })
+
+ objetivo.addEventListener("click", function() {
+
+	window.scrollTo(0, 420)
+	
+ })
+
+ contato.addEventListener("click", function() {
+
+	window.scrollTo(0, 420)
+	
+ })
+
+
